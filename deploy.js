d6b77b34fe31e3fc1cab49c4846450c8cce2bbd9
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-06-07 23:59:55 +0530

    code added

--- deploy.js
+++ deploy.js
@@ -6,11 +6,16 @@ const provider = new HDWalletProvider(
   'https://rinkeby.infura.io/v3/1bb2b0b14ee243e1a15080bccfd8c69c'
 );
 
+let lottery;
 let accounts;
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   accounts = await web3.eth.getAccounts();
   console.log(accounts);
+
+  lottery = await web3.eth.Contract(JSON.parse(interface))
+    .deploy({data:'0x'+bytecode})
+    .send({from: accoun[0], gas:'1000000'})
 }
 deploy();
